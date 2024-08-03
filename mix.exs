7ccf40b69e31468719bb99eaba50a13389e25fa5
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2024-08-03 16:43:24 +0300

    daisy ui initialized

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule GigaChad.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
     ]
   end
 
